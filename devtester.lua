2c0ef8aa2f8e8bd4dd7f16613e336f82a2d91754
Randy Bimm <Bimmr6696@gmail.com>
2022-07-22 10:18:39 -0400

    Working on DevTester

--- devtester.lua
+++ devtester.lua
@@ -1,33 +1,40 @@
-local calls = {{
+-- Seperate calls, gets, sets
+-- Show the result of each call and get 
+-- Maybe seperate setting value and got value, then I can set the value from the get methods instead of returning the value - maybe use status
+local calls = {
+--     {
+--     type = 1,
+--     path = "snow.data.FacilityDataManager",
+--     instructions = {{
+--         type = 2,
+--         name = "_Kitchen",
+--         operation = 1
+--     }, {
+--         type = 2,
+--         name = "_MealFunc",
+--         operation = 1
+--     }, {
+--         type = 2,
+--         name = "<AvailableDangoList>k__BackingField",
+--         operation = 1
+--     }, {
+--         type = 1,
+--         name = "get_Item",
+--         args = 1,
+--         operation = 1
+--     }, {
+--         type = 2,
+--         name = "_Param",
+--         operation = 1
+--     }, {
+--         type = 2,
+--         name = "_Id",
+--         operation = 1
+--     }}
+-- }, 
+{
     path = "snow.data.FacilityDataManager",
-    instructions = {{
-        type = 2,
-        name = "_Kitchen",
-        operation = 1
-    }, {
-        type = 2,
-        name = "_MealFunc",
-        operation = 1
-    }, {
-        type = 2,
-        name = "<AvailableDangoList>k__BackingField",
-        operation = 1
-    }, {
-        type = 1,
-        name = "get_Item",
-        args = 1,
-        operation = 1
-    }, {
-        type = 2,
-        name = "_Param",
-        operation = 1
-    }, {
-        type = 2,
-        name = "_Id",
-        operation = 1
-    }}
-}, {
-    path = "snow.data.FacilityDataManager",
+    type = 1,
     instructions = {{
         type = 2,
         name = "_Kitchen",
@@ -43,35 +50,16 @@ local calls = {{
         operation = 2,
         value = "true"
     }}
-}, {
-    -- local typeOptions = {"Method", "Field", "ArrayIndex"}
-    -- local operationOptions = {"Get", "Set", "Call"}
-    path = "snow.player.PlayerManager",
-    instructions = {{
-        type = 2,
-        name = "PlayerList",
-        operation = 1
-    }, {
-        type = 3,
-        name = "0",
-        operation = 1
-    }, {
-        type = 2,
-        name = "_ShotDamageUpDurationTimer",
-        operation = 1
-    }}
-}}
-
-local hooks = {{
-    path = "snow.facility.kitchen.MealFunc",
+},{
+    path = "snow.player.InsectGlaive",
+    type = 2,
     method = "update",
     time = 1,
     prehook = 1,
     instructions = {{
         type = 2,
-        name = "_MealTicketFlag",
-        operation = 2,
-        value = true
+        name = "_RedExtractiveTime",
+        operation = 1
     }}
 }}
 
@@ -88,12 +76,17 @@ end
 -- local typeOptions = {"Method", "Field", "ArrayIndex"}
 -- local operationOptions = {"Get", "Set", "Call"}
 local function performInstructions(last, instructions, instructionsToPerform)
-    if not last then return "Error with Singleton" end
-    if not instructionsToPerform then
-        instructionsToPerform = #instructions
-    end
+    if not last then 
+            instructions[#instructions].status = "Path not found"
+            return "Path not found"
+        end
+        if not instructionsToPerform then
+            instructionsToPerform = #instructions
+        end
     for i, instruction in ipairs(instructions) do
-        if i > instructionsToPerform then break end
+        if i > instructionsToPerform then
+            break
+        end
         if instruction.type == 1 then
             local args = nil
             if not (instruction.args == nil or instruction.args == "") then args = instruction.args end
@@ -127,17 +120,42 @@ local function performInstructions(last, instructions, instructionsToPerform)
 end
 
 local function initHook(hook)
-    local path = hook.path
-    local method = hook.method
-    local time = hook.time
-    local prehook = hook.prehook
     local instructions = hook.instructions
-    local hook = sdk.find_type_definition(path):get_method(method)
-    sdk.hook(hook, function(args)
-        if time == 1 then end
-        if prehook == 1 then return sdk.PreHookResult.SKIP_ORIGINAL end
+    local path = sdk.find_type_definition(hook.path)
+    if not path then
+        hook.status = "Path not found"
+        hook.isHooked = false
+        return
+    end
+    path = path:get_method(hook.method)
+    if not path then
+        hook.status = "Method not found"
+        hook.isHooked = false
+        return
+    end
+    sdk.hook(path, function(args)
+        local managed = sdk.to_managed_object(args[2])
+        if not managed then return end
+        if not managed:get_type_definition():is_a(hook.path) then
+            return
+        end
+        if hook.time == 1 then
+            local value = performInstructions(managed, instructions)
+            instructions[#instructions].value = value
+        else
+            hook.managed = managed
+        end
+        
+        if hook.prehook == 2 then return sdk.PreHookResult.SKIP_ORIGINAL end
     end, function(retval)
-        if time == 2 then end
+        if hook.managed then 
+            hook.managed = nil
+            if hook.time == 2 then 
+                local value = performInstructions(hook.managed, instructions)
+                instructions[#instructions].value = value
+            end
+        end
+        return retval
     end)
 end
 
@@ -149,10 +167,29 @@ local function drawCallMenu()
         -- if imgui.tree_node("Call #" .. i) then
         if imgui.collapsing_header("Call #" .. i) then
             imgui.spacing()
-            imgui.begin_rect()
-            changed, call.path = imgui.input_text("Managed Singleton " .. i, call.path)
-            local singleton = sdk.get_managed_singleton(call.path)
-            imgui.end_rect(5, 1)
+            if call.isHooked then 
+                imgui.text("[Hooked]")
+            else
+                changed, call.type = imgui.combo("Type " .. i, call.type, {"Singleton", "Hook"}) 
+            end
+            changed, call.path = imgui.input_text("Path " .. i, call.path)
+            if call.type == 2 then
+                if not call.isHooked then
+                    imgui.same_line()
+                    imgui.text("  ")
+                    imgui.same_line()
+                    if imgui.button("[Create Hook]") then
+                        call.isHooked = true
+                        initHook(call)
+                    end
+                end
+                
+            changed, call.method = imgui.input_text("Method " .. i, call.method)
+            changed, call.time = imgui.combo("Time " .. i, call.time, {"Pre", "Post"})
+            changed, call.prehook = imgui.combo("PreHookResult " .. i, call.prehook, {"CALL_ORIGINAL", "SKIP_ORIGINAL"})
+            end
+            local path = sdk.find_type_definition(call.path)
+            if call.type == 1 then path = sdk.get_managed_singleton(call.path) end
             imgui.spacing()
             imgui.spacing()
             for i1, instruction in ipairs(call.instructions) do
@@ -160,7 +197,7 @@ local function drawCallMenu()
                 imgui.begin_rect()
                 local typeOptions = {"Method", "Field", "ArrayIndex"}
                 local operationOptions = {"Get", "Set", "Call"}
-                if instruction.type ~= 1 then table.remove(operationOptions, #operationOptions) end
+                if instruction.type ~= 1 or call.type == 2 then table.remove(operationOptions, #operationOptions) end
                 changed, instruction.type = imgui.combo("Type " .. i .. "-" .. i1, instruction.type, typeOptions)
                 changed, instruction.operation = imgui.combo("Operation " .. i .. "-" .. i1, instruction.operation,
                                                              operationOptions)
@@ -176,20 +213,26 @@ local function drawCallMenu()
 
                 -- If operation is GET and is last item in instructions, show value
                 if instruction.operation == 1 and i1 == #call.instructions then
-                    imgui.input_text("Value " .. i .. "-" .. i1, performInstructions(singleton, call.instructions, i1),
+                    if call.type == 1 then
+                    imgui.input_text("Value " .. i .. "-" .. i1, performInstructions(path, call.instructions, i1),
                                      ImGuiInputTextFlags_ReadOnly)
+                    else
+                        imgui.input_text("Value " .. i .. "-" .. i1, instruction.value,
+                                         ImGuiInputTextFlags_ReadOnly)
+                    end
 
                     --  If operation is SET, show value input, and a set button
                 elseif instruction.operation == 2 then
                     changed, instruction.value = imgui.input_text("Value " .. i .. "-" .. i1, instruction.value)
                     if imgui.button("Set Value " .. i .. "-" .. i1) then
-                        performInstructions(singleton, call.instructions, i1)
+                        if call.type == 1 then
+                            performInstructions(path, call.instructions)
+                        end
                     end
                     -- If operation is CALL, show value input, and a call button
                 elseif instruction.operation == 3 then
-
                     if imgui.button("Call Method " .. i .. "-" .. i1) then
-                        performInstructions(singleton, call.instructions, i1)
+                        performInstructions(path, call.instructions)
                     end
                 end
                 imgui.spacing()
