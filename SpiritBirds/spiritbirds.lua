93a6ce923bc0fda74f4361c4b55c875ddab4244a
Randy Bimm <Bimmr6696@gmail.com>
2022-07-27 12:42:44 -0400

    Made GUI togglable, added commented code 4 custom

--- SpiritBirds/spiritbirds.lua
+++ SpiritBirds/spiritbirds.lua
@@ -1,16 +1,17 @@
 -- Sprit Bird types w/ index for EnvironmentCreatureManager#_EcPrefabList.mItems
 local SPIRIT_BIRDS = {
-    atk  = 11,
-    def  = 12,
-    hp   = 13,
-    spd  = 14,
-    all  = 15,
+    atk = 11,
+    def = 12,
+    hp = 13,
+    spd = 14,
+    all = 15,
     gold = 31
 }
 local autospawn = {
     enabled = true,
     spawned = false
 }
+local isWindowOpen, wasOpen = false, false
 
 local spawnedBirds = {}
 
@@ -21,7 +22,8 @@ local function loadConfig()
     if json ~= nil then
         local file = json.load_file(configPath)
         if file then
-            autospawn.enabled = file.autospawn
+            autospawn.enabled = file.autospawn or true
+            isWindowOpen = file.isWindowOpen or false
         end
     end
 end
@@ -29,7 +31,10 @@ loadConfig()
 
 -- Save the config
 local function saveConfig()
-    json.dump_file(configPath, {autospawn = autospawn.enabled})
+    json.dump_file(configPath, {
+        autospawn = autospawn.enabled,
+        isWindowOpen = isWindowOpen
+    })
 end
 
 -- Get the player
@@ -40,7 +45,7 @@ local function getPlayer()
     return player
 end
 
---Get the player's location
+-- Get the player's location
 local function getPlayerLocation()
     local player = getPlayer()
     if not player then return end
@@ -66,33 +71,27 @@ end
 -- Function to get length of table
 local function getLength(obj)
     local count = 0
-    for _ in pairs(obj) do
-        count = count + 1
-    end
+    for _ in pairs(obj) do count = count + 1 end
     return count
 end
 
 -- Spawn the bird
 local function spawnBird(type)
-    log.debug("Trying to spawn bird: " .. type)
     local envCreature = getEnvCreatureManager()
     local location = getPlayerLocation()
-    
+
     -- Create the bird
     local ecList = envCreature:get_field("_EcPrefabList"):get_field("mItems"):get_elements()
     local ecBird = ecList[SPIRIT_BIRDS[type]]
-    if not ecBird:call("get_Standby") then
-        ecBird:call("set_Standby", true)
-    end
+    if not ecBird:call("get_Standby") then ecBird:call("set_Standby", true) end
 
     -- Set the bird as active
     local bird = ecBird:call("instantiate(via.vec3)", location)
 
     -- If the bird isn't managed, try spawning another (This prevents the having to spawn twice if bird doesn't exist in level)
-     if not sdk.is_managed_object(bird) then
+    if not sdk.is_managed_object(bird) then
         spawnBird(type)
-     else
-        log.debug("Spawned Bird: " .. type)
+    else
         table.insert(spawnedBirds, bird)
     end
 end
@@ -102,60 +101,52 @@ re.on_pre_application_entry("UpdateBehavior", function()
     -- If Auto spawn is enabled and quest status says it's active
     if getQuestStatus() == 2 and autospawn.enabled and not autospawn.spawned then
         if not autospawn.spawned then
-            log.debug("Auto Spawning Bird")
             spawnBird("all")
+            -- If you want to change the the mod to spawn in 5 birds of a different type, just replace the line above with one of the following
+            -- for i = 0, 5 do     spawnBird("hp")       end
+            -- for i = 0, 5 do     spawnBird("atk")      end
+            -- for i = 0, 5 do     spawnBird("def")      end
+            -- I have no plans to add this as a feature so you can change it if you want
             autospawn.spawned = true
         end
-    
-    -- If the quest status is not active, clear the spawned birds, and set autospawned.spawned to false
+
+        -- If the quest status is not active, clear the spawned birds, and set autospawned.spawned to false
     elseif getQuestStatus() ~= 2 and getLength(spawnedBirds) > 0 then
-        log.debug("Clearing spawned birds")
         autospawn.spawned = false
-        for i, bird in pairs(spawnedBirds) do
-            log.debug("Destroying bird")
-            bird:call("destroy", bird)
-        end
+        for i, bird in pairs(spawnedBirds) do bird:call("destroy", bird) end
         spawnedBirds = {}
     end
 end)
 
 -- Remove any spawned birds on on script reset
 re.on_script_reset(function()
-    log.debug("Script Resetting")
-    for i, bird in pairs(spawnedBirds) do
-        log.debug("Destroying bird")
-        bird:call("destroy", bird)
-    end
+    for i, bird in pairs(spawnedBirds) do bird:call("destroy", bird) end
     spawnedBirds = {}
 end)
 
 -- Draw a window to the REFramework Script Generated UI
 re.on_draw_ui(function()
-    imgui.begin_window("Spawn SpiritBirds", ImGuiWindowFlags_AlwaysAutoResize)
-    if imgui.button("   « Attack »    ") then 
-        spawnBird("atk")
-    end
-    imgui.same_line()
-    if imgui.button("   « Defense »   ") then 
-        spawnBird("def")
-    end
-    if imgui.button("   « Health »    ") then 
-        spawnBird("hp")
-    end
-    imgui.same_line()
-    if imgui.button("   « Stamina »   ") then 
-        spawnBird("spd")
-    end
-    if imgui.button("                 « Rainbow »                  ") then 
-        spawnBird("all")
-    end
-    if imgui.button("                  « Golden »                    ") then 
-        spawnBird("gold")
-    end
-    local changed = false
-    changed, autospawn.enabled = imgui.checkbox("Auto-Spawn Prism", autospawn.enabled)
-    if changed then
+    if imgui.button("Toggle SpiritBird GUI") then
+        isWindowOpen = not isWindowOpen
         saveConfig()
     end
-    imgui.end_window()
+    if isWindowOpen then
+        wasOpen = true
+        isWindowOpen = imgui.begin_window("Spawn SpiritBirds", isWindowOpen, 64)
+        if imgui.button("   « Attack »    ") then spawnBird("atk") end
+        imgui.same_line()
+        if imgui.button("   « Defense »   ") then spawnBird("def") end
+        if imgui.button("   « Health »    ") then spawnBird("hp") end
+        imgui.same_line()
+        if imgui.button("   « Stamina »   ") then spawnBird("spd") end
+        if imgui.button("                 « Rainbow »                  ") then spawnBird("all") end
+        if imgui.button("                  « Golden »                    ") then spawnBird("gold") end
+        local changed = false
+        changed, autospawn.enabled = imgui.checkbox("Auto-Spawn Prism", autospawn.enabled)
+        if changed then saveConfig() end
+        imgui.end_window()
+    elseif wasOpen then
+        wasOpen = false
+        saveConfig()
+    end
 end)
