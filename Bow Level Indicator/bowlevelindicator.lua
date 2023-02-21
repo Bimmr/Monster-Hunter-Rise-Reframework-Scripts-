9ff785ba27103bb96641b42643c0ff04b5dd8197
Randy Bimm <Bimmr6696@gmail.com>
2023-02-21 14:49:30 -0500

    Cleanup

--- Bow Level Indicator/bowlevelindicator.lua
+++ Bow Level Indicator/bowlevelindicator.lua
@@ -1,19 +1,7 @@
+local musicManager, questManager
+
 -- Settings for text
 local settings = {
-    textSettings = {
-        enabled = true,
-        textColour = "0xFFFFFFFF",
-        location = {
-            x = 153,
-            y = 125
-        },
-        levels = {
-            [0] = "0xd60000FF",
-            [1] = "0xA7A3D5F4",
-            [2] = "0xffffb224",
-            [3] = "0xFF00FF00"
-        }
-    },
     -- Settings for icons
     iconSettings = {
         enabled = true,
@@ -47,13 +35,6 @@ local function saveConfig()
     json.dump_file(configPath, settings)
 end
 
--- Get Quest Status
-local function getQuestState()
-    local quest = sdk.get_managed_singleton("snow.QuestManager")
-    if not quest then return end
-    return quest:get_field("_QuestStatus")
-end
-
 -- Function to get the window size
 local function getWindowSize()
     local sceneManager = sdk.get_native_singleton("via.SceneManager");
@@ -71,14 +52,54 @@ local function getWindowSize()
         height = lheight
     }
 end
+-- Check if player is in battle, code by raffRun
+local function checkIfInBattle()
+
+    if not musicManager then musicManager = sdk.get_managed_singleton("snow.wwise.WwiseMusicManager") end
+    if not questManager then questManager = sdk.get_managed_singleton("snow.QuestManager") end
+
+    local currentBattleState = musicManager:get_field("_CurrentEnemyAction")
+    local currentMixUsed = musicManager:get_field("_Current")
+
+    local currentQuestType = questManager:get_field("_QuestType")
+    local currentQuestStatus = questManager:get_field("_QuestStatus")
+
+    local inBattle = currentBattleState == 3 -- Fighting a monster
+    or currentMixUsed == 37 -- Fighting a wave of monsters
+    or currentMixUsed == 10 -- Stronger battle music mix is being used
+    or currentMixUsed == 31 -- Used in some arena battles
+    or currentQuestType == 64 -- Fighting in the arena (Utsushi)
+
+    local isQuestComplete = currentQuestStatus == 3 -- Completed the quest
+    or currentQuestStatus == 0 -- Not in a quest
+
+    return inBattle and not isQuestComplete
+end
+
+local function getPlayer()
+    return sdk.get_managed_singleton("snow.player.PlayerManager"):call("findMasterPlayer")
+end
+
+local function isWeaponSheathed()
+    local player = getPlayer()
+    local playerAction = sdk.find_type_definition("snow.player.PlayerBase"):get_field("<RefPlayerAction>k__BackingField"):get_data(player)
+    return sdk.find_type_definition("snow.player.PlayerAction"):get_field("_weaponFlag"):get_data(playerAction) == 0
+end
+
+local function isBow()
+    local player = getPlayer()
+    return player:get_field("_playerWeaponType") == 13
+end
+local function isAiming()
+    local player = getPlayer()
+    return player:get_field("<IsAimMode>k__BackingField")
+end
 
 -- Hook to get the Bow Level
-sdk.hook(sdk.get_managed_singleton("snow.StmInputManager"):get_field("_InGameInputDevice"):get_field("_pl_input")
-             :get_method("update"), function(args)
+sdk.hook(sdk.find_type_definition("snow.player.Bow"):get_method("update"), function(args)
     local bow = sdk.to_managed_object(args[2])
     if not bow then return end
-    if not type(bow) == sdk.typeof("snow.player.Bow") then return end
-    level = bow:get_field("<ChargeLv>k__BackingField")
+    level = bow:get_field("<ChargeLv>k__BackingField") + 1
 end, function(retval)
 end)
 
@@ -86,58 +107,35 @@ re.on_frame(function()
     if screenSize.width == 0 then getWindowSize() end
 
     -- TODO: Find a way to check if the player is aiming, and use that instead, could also check what weapon type is being held
-    if getQuestState() == 2 then
+    if isBow() and isAiming() then
 
-        -- Draw text level
-        if settings.textSettings.enabled then
-            draw.text("Bow Level: ", settings.textSettings.location.x + 1, settings.textSettings.location.y + 1,
-                      "0xFF000000")
-            draw.text("Bow Level: ", settings.textSettings.location.x, settings.textSettings.location.y,
-                      settings.textSettings.textColour)
+            local lastX = screenSize.width / 2 - settings.iconSettings.size / 2 - (settings.iconSettings.size * (level + 1) / 2) - (settings.iconSettings.spacing * (level + 1) / 2)
+            for i = 1, level do
+                local x = lastX + settings.iconSettings.size + settings.iconSettings.spacing
+                lastX = x
 
-            draw.text(level, settings.textSettings.location.x + 70 + 1, settings.textSettings.location.y + 1,
-                      "0xFF000000")
-            draw.text(level, settings.textSettings.location.x + 70, settings.textSettings.location.y,
-                      settings.textSettings.levels[level])
-        end
+                -- Draw a square
+                if settings.iconSettings.shape == 2 then
+                    draw.filled_rect(x, screenSize.height / 2 - settings.iconSettings.size / 2, settings.iconSettings.size, settings.iconSettings.size, settings.iconSettings.colour)
+                    draw.outline_rect(x, screenSize.height / 2 - settings.iconSettings.size / 2, settings.iconSettings.size, settings.iconSettings.size,
+                                      settings.iconSettings.colour)
 
-        -- Draw Icons
-        if settings.iconSettings.enabled then
-            if level > 0 then
-                local lastX = screenSize.width / 2 - settings.iconSettings.size / 2 -
-                                  (settings.iconSettings.size * (level + 1) / 2) -
-                                  (settings.iconSettings.spacing * (level + 1) / 2)
-                for i = 0, level - 1 do
-                    local x = lastX + settings.iconSettings.size + settings.iconSettings.spacing
-                    lastX = x
-
-                    -- Draw a square
-                    if settings.iconSettings.shape == 2 then
-                        draw.filled_rect(x, screenSize.height / 2 - settings.iconSettings.size / 2,
-                                         settings.iconSettings.size, settings.iconSettings.size,
-                                         settings.iconSettings.colour)
-                        draw.outline_rect(x, screenSize.height / 2 - settings.iconSettings.size / 2,
-                                          settings.iconSettings.size, settings.iconSettings.size,
-                                          settings.iconSettings.colour)
-
-                        -- Draw a circle
-                    elseif settings.iconSettings.shape == 1 then
-                        x = x + settings.iconSettings.size / 2
-                        local y = screenSize.height / 2
-                        local radius = settings.iconSettings.size / 2
-                        local angle = 0
-                        local step = math.pi * 2 / settings.iconSettings.size
-                        for i = 0, settings.iconSettings.size do
-                            local x1 = x + radius * math.cos(angle)
-                            local y1 = y + radius * math.sin(angle)
-                            local x2 = x + radius * math.cos(angle + step)
-                            local y2 = y + radius * math.sin(angle + step)
-                            draw.line(x1, y1, x2, y2, settings.iconSettings.colour)
-                            angle = angle + step
-                        end
+                    -- Draw a circle
+                elseif settings.iconSettings.shape == 1 then
+                    x = x + settings.iconSettings.size / 2
+                    local y = screenSize.height / 2
+                    local radius = settings.iconSettings.size / 2
+                    local angle = 0
+                    local step = math.pi * 2 / settings.iconSettings.size
+                    for i = 0, settings.iconSettings.size do
+                        local x1 = x + radius * math.cos(angle)
+                        local y1 = y + radius * math.sin(angle)
+                        local x2 = x + radius * math.cos(angle + step)
+                        local y2 = y + radius * math.sin(angle + step)
+                        draw.line(x1, y1, x2, y2, settings.iconSettings.colour)
+                        angle = angle + step
                     end
                 end
-            end
         end
     end
 end)
@@ -146,38 +144,8 @@ re.on_draw_ui(function()
     local changed = false
 
     if (imgui.button("Bow Level Indicator")) then configure = true end
-    if configure and imgui.begin_window("Bow Level Indicator Config", true, ImGuiWindowFlags_AlwaysAutoResize) then
-        if imgui.tree_node("[Text]") then
-
-            changed, settings.textSettings.enabled = imgui.checkbox("Enabled", settings.textSettings.enabled)
-            if changed then saveConfig() end
-            imgui.text("Position")
-            changed, settings.textSettings.location.x = imgui.drag_int("X", settings.textSettings.location.x, 1, 0,
-                                                                       screenSize.width)
-            if changed then saveConfig() end
-            changed, settings.textSettings.location.y = imgui.drag_int("Y", settings.textSettings.location.y, 1, 0,
-                                                                       screenSize.height)
-            if changed then saveConfig() end
-            if imgui.tree_node("Text Colour") then
-                changed, settings.textSettings.textColour = imgui.color_picker("Text Colour",
-                                                                               settings.textSettings.textColour)
-                if changed then saveConfig() end
-                imgui.tree_pop()
-            end
-
-            for i = 0, 3 do
-                if imgui.tree_node("Level " .. i) then
-                    changed, settings.textSettings.levels[i] =
-                        imgui.color_picker("Level " .. i, settings.textSettings.levels[i])
-                    if changed then saveConfig() end
-                    imgui.tree_pop()
-                end
-            end
-
-            imgui.tree_pop()
-        end
-
-        if imgui.tree_node("[Icon]") then
+    if configure and imgui.begin_window("Bow Level Indicator Config", true, 0) then
+       
             changed, settings.iconSettings.enabled = imgui.checkbox("Enabled", settings.iconSettings.enabled)
             if changed then saveConfig() end
             local possibleShapes = {"Circle", "Square"}
@@ -193,15 +161,9 @@ re.on_draw_ui(function()
             if changed then saveConfig() end
             changed, settings.iconSettings.spacing = imgui.drag_int("Spacing", settings.iconSettings.spacing, 1, 0, 20)
             if changed then saveConfig() end
-            changed, settings.iconSettings.offset = imgui.drag_int("Offset", settings.iconSettings.offset, 1,
-                                                                   screenSize.height / 2 * -1, screenSize.height / 2)
-            if changed then saveConfig() end
+            
 
-            imgui.tree_pop()
-        end
 
         imgui.end_window()
-    else
-        configure = false
     end
 end)
