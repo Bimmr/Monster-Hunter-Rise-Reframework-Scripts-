b1eaf8192643564c325fa0c3b7f93386e7d1ab07
Bimmr <Bimmr6696@gmail.com>
2024-05-16 10:41:32 -0400

    For now disabled all dango

--- Buffer/autorun/Buffer/Modules/Miscellaneous.lua
+++ Buffer/autorun/Buffer/Modules/Miscellaneous.lua
@@ -16,7 +16,7 @@ local data = {
         dango_100_no_ticket = false,
         dango_100_ticket = false,
         level_4 = false,
-        all_dangos = false
+        -- all_dangos = false
     },
     unlimited_recon = false,
     hidden = {
@@ -148,18 +148,18 @@ function data.init_hooks()
         end
     end, utils.nothing())
   
-    sdk.hook(sdk.find_type_definition("snow.facility.kitchen.MealFunc"):get_method("updateList"), function(args)
-      local managed = sdk.to_managed_object(args[2])
-      if not managed then return end
-      local dangoList = managed:get_field("<DangoDataList>k__BackingField"):get_field("mItems")
-      for i, dango in pairs(dangoList) do
-          local dangoParam = dango:get_field("_Param")
-          -- Set unlock Flag to Village_1 and Dailyrate to 0
-          dangoParam:set_field("_UnlockFlag", 5)
-          dangoParam:set_field("_DailyRate", 0)
-      end
-      managed:set_field("<AvailableDangoList>k__BackingField", managed:get_field("<DangoDataList>k__BackingField"))
-    end, utils.nothing())
+    -- sdk.hook(sdk.find_type_definition("snow.facility.kitchen.MealFunc"):get_method("updateList"), function(args)
+    --   local managed = sdk.to_managed_object(args[2])
+    --   if not managed then return end
+    --   local dangoList = managed:get_field("<DangoDataList>k__BackingField"):get_field("mItems")
+    --   for i, dango in pairs(dangoList) do
+    --       local dangoParam = dango:get_field("_Param")
+    --       -- Set unlock Flag to Village_1 and Dailyrate to 0
+    --       dangoParam:set_field("_UnlockFlag", 5)
+    --       dangoParam:set_field("_DailyRate", 0)
+    --   end
+    --   managed:set_field("<AvailableDangoList>k__BackingField", managed:get_field("<DangoDataList>k__BackingField"))
+    -- end, utils.nothing())
 
     local recon_managed = nil
     sdk.hook(sdk.find_type_definition("snow.otomo.OtomoReconCharaManager"):get_method("onCompleteReconOtomoAct"), function(args)
@@ -223,8 +223,8 @@ function data.draw()
             any_changed = any_changed or changed
             changed, data.canteen.level_4 = imgui.checkbox(language.get(languagePrefix .. "level_4"), data.canteen.level_4)
             any_changed = any_changed or changed
-            changed, data.canteen.all_dangos = imgui.checkbox(language.get(languagePrefix .. "all_dangos"), data.canteen.all_dangos)
-            utils.tooltip(language.get(languagePrefix.."all_dangos_tooltip"))
+            -- changed, data.canteen.all_dangos = imgui.checkbox(language.get(languagePrefix .. "all_dangos"), data.canteen.all_dangos)
+            -- utils.tooltip(language.get(languagePrefix.."all_dangos_tooltip"))
             any_changed = any_changed or changed
             imgui.tree_pop()
         end
